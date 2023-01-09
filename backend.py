bc824d17ce680155f31170015fc2df21dce26080
Milan <kricka.milan@gmail.com>
2023-01-09 14:42:10 +0100

    Final version of the app, fully functional

--- backend.py
+++ backend.py
@@ -1,25 +1,12 @@
 import requests
 
-def get_data(place,option,days=5):
+def get_data(place,days):
     api_key="042c0a53137470bd1fd3afb3cf203838"
     url=f"http://api.openweathermap.org/data/2.5/forecast?q={place.title()}&appid={api_key}"
     response=requests.get(url)
     data=response.json()
     filtered_data=data['list'][:8*days]
-    match option:
-        case "Temperature":
-            for item in filtered_data:
-                a=[item['dt_txt'] for item in filtered_data]
-                b=[item['main']['temp'] for item in filtered_data]
-            return a,b
-
-        case "Humidity":
-            for item in filtered_data:
-                a=[item['dt_txt'] for item in filtered_data]
-                b=[item['main']['humidity'] for item in filtered_data]
-            return a,b
-
-get_data("Belgrade","Temperature",4)
+    return filtered_data
 
 if __name__=="__main__":
-    print(get_data(place="Tokyo",days=None,option=None))
+    print(get_data(place="Tokyo",days=3,option="Sky"))
--- main.py
+++ main.py
@@ -2,19 +2,54 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+
+# Add title, , place, days and kind of presentation
 st.title("Weather Forecast for the Next Days")
 
 place=st.text_input(label='Place')
 
 days=st.slider(label="Forecast Days",min_value=1,max_value=5,step=1)
 
-option=st.selectbox(label='Select data to view',options=['Temperature','Humidity'])
+option=st.selectbox(label='Select data to view',options=['Temperature','Humidity','Sky'])
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-a,b=get_data(place,option,days)
-figure = px.line(x=a, y=b, labels={"x": "Date", "y": f"{option.title()}"})
-st.plotly_chart(figure)
+
+if place:
+
+
+# Plot the chart
+
+    try:
+        # Get the temperature,humidity or sky data
+        filtered_data = get_data(place, days)
+        if option == "Temperature":
+            dates=[item['dt_txt'] for item in filtered_data]
+            temps=[item['main']['temp']/10 for item in filtered_data]
+            figure = px.line(x=dates, y=temps, labels={"x": "Date", "y": f"{option.title()}"})
+            st.plotly_chart(figure)
+
+        elif option=="Humidity":
+            dates = [item['dt_txt'] for item in filtered_data]
+            hum = [item['main']['humidity'] for item in filtered_data]
+            figure = px.line(x=dates, y=hum, labels={"x": "Date", "y": f"{option.title()}"})
+            st.plotly_chart(figure)
+
+        elif option=="Sky":
+            dates = [item['dt_txt'] for item in filtered_data]
+            skies=[item['weather'][0]['main'] for item in filtered_data]
+            images = {"Clouds": "images/clouds.png", "Clear": "images/clear.png", "Rain": 'images/rain.png',
+                      "Snow": "images/snow.png"}
+            img=[images[sky] for sky in skies]
+            st.image(img,width=115,caption=skies)
+    except KeyError:
+        st.warning("This city doesn't exist", icon="⚠️")
+
+
+
+
+
+
 
 
 
