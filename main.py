4c5dba46d5f8a056a3dd940e9b8bd29bca30c535
Milan <kricka.milan@gmail.com>
2023-01-09 13:05:12 +0100

    Gettign data from API and presenting it on webapp

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 import streamlit as st
 import plotly.express as px
-import requests
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 
-
-st.text_input(label='Place')
-
+place=st.text_input(label='Place')
 
 days=st.slider(label="Forecast Days",min_value=1,max_value=5,step=1)
 
-
 option=st.selectbox(label='Select data to view',options=['Temperature','Humidity'])
 
-st.subheader(f"{option} for the next {days} days in Tirana")
+st.subheader(f"{option} for the next {days} days in {place}")
 
-
-
-def get_data(days):
-    dates=["2022-25-10","2022-26-10","2022-27-10",]
-    temperatures=[10,11,12]
-    temperatures=[days*i for i in temperatures]
-    return dates,temperatures
-
-d,t=get_data(days)
-
-figure=px.line(x=d,y=t,labels={"x":"Date","y":"Temperature in (C)"})
+a,b=get_data(place,option,days)
+figure = px.line(x=a, y=b, labels={"x": "Date", "y": f"{option.title()}"})
 st.plotly_chart(figure)
+
+
+
+
+
